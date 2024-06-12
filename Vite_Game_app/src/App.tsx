bdd5c3a499e026415e639d8c478bebc6fa7ffcd4
Mahedere <mahederetebebe98@gmail.com>
2024-06-12 08:14:19 -0700

    Button feature added

--- Vite_Game_app/src/App.tsx
+++ Vite_Game_app/src/App.tsx
@@ -1,6 +1,8 @@
+import Button from "./components/Button"
 import ListGroup from "./components/ListGroup"
 function App(){
   return <div><ListGroup></ListGroup>
+  <Button>ha</Button>
   </div>
 }
 export default App
--- Vite_Game_app/src/components/ListGroup.tsx
+++ Vite_Game_app/src/components/ListGroup.tsx
@@ -5,7 +5,7 @@ function ListGroup() {
             <h1>List</h1>
             <ul className="list-group">
                 {items.map((item) => (
-                    <li key={item} onClick={(event) => console.log(event)}>{item}</li>))}
+                <li key={item} onClick={(event)=>console.log(event)}>{item}</li>))}
             </ul>
         </>
     );
